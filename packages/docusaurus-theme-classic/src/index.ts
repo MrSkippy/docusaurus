1bb6fec82b043ceac94f94651ce58d47f7df5526
Jaap-Hein Wester <MrSkippy@users.noreply.github.com>
2025-07-03 16:45:40 +0200

    Strict CSP error: no inline styles allowed
    Only way to fix is to use a `class` or `unsafe-hashes`, the lather not an option if you need a strict Content Security Policy in your project (e.g. government audited).

--- packages/docusaurus-theme-classic/src/index.ts
+++ packages/docusaurus-theme-classic/src/index.ts
@@ -126,7 +126,7 @@ export default function themeClassic(
             tagName: 'svg',
             attributes: {
               xmlns: 'http://www.w3.org/2000/svg',
-              style: 'display: none;',
+              class: 'hidden',
             },
             innerHTML: SvgSpriteDefs,
           },
